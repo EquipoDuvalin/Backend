5af05ea3a3c50f6b3a989b5c78e138b493489737
Briseida <briseidaruizdelacruz@gmail.com>
2022-10-07 17:19:33 -0500

    #Fix 7

---
+++ src/test/java/com/losquemonitosdechill/Backend/proveedor/ProveedorControllerTest.java
@@ -0,0 +1,133 @@
+package com.losquemonitosdechill.Backend.proveedor;
+
+import java.sql.Date;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+
+public class ProveedorControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Autowired
+  private ProveedorService proveedorService;
+
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  private Proveedor proveedorOne = new Proveedor();
+  private Proveedor proveedorTwo = new Proveedor();
+
+  @Test
+  void create() throws Exception {
+
+    proveedorOne.setRfc("rucb0104");
+    proveedorOne.setRazon_social("rucb010415");
+    proveedorOne.setNombre_contacto("briseida");
+    proveedorOne.setTel_principal("58913422");
+    proveedorOne.setTel_movil("5561025921");
+    proveedorOne.setE_mail("brissdelacruz@gmail.com");
+    proveedorOne.setEstatus("activo");
+    proveedorOne.setFecha_registro(Date.valueOf("2022-10-05"));
+
+    ResultActions response = mockMvc.perform(post("/api/proveedor/add")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(objectMapper.writeValueAsString(proveedorOne)));
+
+    response.andDo(print()).andExpect(status().isCreated())
+        .andExpect(jsonPath("$.id_proveedor", is(proveedorOne.getId_proveedor())))
+
+        .andExpect(jsonPath("$.rfc", is(proveedorOne.getRfc())))
+
+        .andExpect(jsonPath("$.razon_social", is(proveedorOne.getRazon_social())))
+        .andExpect(jsonPath("$.nombre_contacto", is(proveedorOne.getNombre_contacto())))
+        .andExpect(jsonPath("$.tel_principal", is(proveedorOne.getTel_principal())))
+        .andExpect(jsonPath("$.tel_movil", is(proveedorOne.getTel_movil())))
+        .andExpect(jsonPath("$.e_email", is(proveedorOne.getE_mail())))
+        .andExpect(jsonPath("$.estatus", is(proveedorOne.getEstatus())))
+        .andExpect(jsonPath("$.fecha_registro", is(Date.valueOf("2022-10-04").toString())));
+  }
+
+  @Test
+  void getAll() throws Exception {
+    ResultActions response = mockMvc.perform(get("/api/proveedor/list"));
+    response.andExpect(status().isOk())
+        .andDo(print());
+
+  }
+
+  @Test
+  void deleteById() throws Exception {
+    // Setup
+    proveedorTwo.setRfc("rucb0104");
+    proveedorTwo.setRazon_social("rucb010415");
+    proveedorTwo.setNombre_contacto("briseida");
+    proveedorTwo.setTel_principal("58913422");
+    proveedorTwo.setTel_movil("5561025921");
+    proveedorTwo.setE_mail("brissdelacruz@gmail.com");
+    proveedorTwo.setEstatus("activo");
+    proveedorTwo.setFecha_registro(Date.valueOf("2022-10-05"));
+
+    proveedorService.create(proveedorOne);
+    ResultActions response = mockMvc.perform(delete("/api/proveedor/delete/{id}", proveedorTwo.getId_proveedor()));
+    response.andExpect(status().isOk());
+
+  }
+
+  @Test
+  void getById() throws Exception {
+    // Setup
+
+    proveedorTwo.setRfc("rucb0104");
+    proveedorTwo.setRazon_social("rucb010415");
+    proveedorTwo.setNombre_contacto("briseida");
+    proveedorTwo.setTel_principal("58913422");
+    proveedorTwo.setTel_movil("5561025921");
+    proveedorTwo.setE_mail("brissdelacruz@gmail.com");
+    proveedorTwo.setEstatus("activo");
+    proveedorTwo.setFecha_registro(Date.valueOf("2022-10-05"));
+
+    ResultActions response = mockMvc.perform(get("/api/venta/{id}", proveedorOne.getId_proveedor()));
+
+    response.andDo(print()).andExpect(status().isOk())
+        .andExpect(jsonPath("$.id_proveedor", is(proveedorOne.getId_proveedor())))
+
+        .andExpect(jsonPath("$.rfc", is(proveedorOne.getRfc())))
+
+        .andExpect(jsonPath("$.razon_social", is(proveedorOne.getRazon_social())))
+        .andExpect(jsonPath("$.nombre_contacto", is(proveedorOne.getNombre_contacto())))
+        .andExpect(jsonPath("$.tel_principal", is(proveedorOne.getTel_principal())))
+        .andExpect(jsonPath("$.tel_movil", is(proveedorOne.getTel_movil())))
+        .andExpect(jsonPath("$.e_email", is(proveedorOne.getE_mail())))
+        .andExpect(jsonPath("$.estatus", is(proveedorOne.getEstatus())))
+        .andExpect(jsonPath("$.fecha_registro", is(Date.valueOf("2022-10-04").toString())));
+  }
+
+}
